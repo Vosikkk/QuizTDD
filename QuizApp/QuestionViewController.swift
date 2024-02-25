4a122d05d0e494ead244e16cca90a6ac42fc33d2
Vosik <vosikvos@gmail.com>
2024-02-25 22:43:24 +0200

    added helpers, made UI

--- QuizApp/QuestionViewController.swift
+++ QuizApp/QuestionViewController.swift
@@ -16,11 +16,11 @@ class QuestionViewController: UIViewController, UITableViewDataSource, UITableVi
     
     private var question: String = ""
     private var options: [String] = []
-    private var selection: ((String) -> Void)? = nil
+    private var selection: (([String]) -> Void)? = nil
     private let reuseIdentifier: String = "Cell"
     
     
-    convenience init(question: String, options: [String], selection: @escaping (String) -> Void) {
+    convenience init(question: String, options: [String], selection: @escaping ([String]) -> Void) {
         self.init()
         self.question = question
         self.options = options
@@ -52,9 +52,20 @@ class QuestionViewController: UIViewController, UITableViewDataSource, UITableVi
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selection?(options[indexPath.row])
+        selection?(selectedOptions(in: tableView))
     }
     
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if tableView.allowsMultipleSelection {
+            selection?(selectedOptions(in: tableView))
+        }
+    }
+    
+    private func selectedOptions(in tableView: UITableView) -> [String] {
+        guard let indexPaths = tableView.indexPathsForSelectedRows else { return [] }
+        return indexPaths.map { options[$0.row] }
+    }
     
     private func dequeueCell(in tableView: UITableView) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) {
