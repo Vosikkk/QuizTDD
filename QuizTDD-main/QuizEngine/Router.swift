2ad6974c21554cc7de8aa30126a47b1f9bf7bcaa
Vosik <vosikvos@gmail.com>
2024-03-12 00:12:56 +0200

    R!! - Introduce the new QuizDelegate Protocol (it will replace the Router Protocol)

--- QuizTDD-main/QuizEngine/Router.swift
+++ QuizTDD-main/QuizEngine/Router.swift
@@ -7,11 +7,23 @@
 
 import Foundation
 
+public protocol QuizDelegate {
+    
+    associatedtype Question: Hashable
+    associatedtype Answer
+    
+    func handle(question: Question, answerCallback: @escaping (Answer) -> Void)
+    func handle(result: Result<Question, Answer>)
+}
+
+
 
 @available(*, deprecated)
 public protocol Router {
+    
     associatedtype Question: Hashable
     associatedtype Answer
+    
     func routeTo(question: Question, answerCallback: @escaping (Answer) -> Void)
     func routeTo(result: Result<Question, Answer>)
 }
