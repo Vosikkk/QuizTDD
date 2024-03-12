bc6ad699088f5bee901d182f5ac7797ef777a45d
Vosik <vosikvos@gmail.com>
2024-03-13 00:40:00 +0200

    r - move Quiz to its own file

---
+++ QuizTDD-main/QuizEngine/Quiz.swift
@@ -0,0 +1,23 @@
+//
+//  Quiz.swift
+//  QuizEngine
+//
+//  Created by Саша Восколович on 13.03.2024.
+//
+
+import Foundation
+
+public final class Quiz {
+    
+    private let flow: Any
+    
+    init(flow: Any) {
+        self.flow = flow
+    }
+    
+    public static func start<Question, Answer: Equatable, Delegate: QuizDelegate>(questions: [Question], delegate: Delegate,  correctAnswers: [Question: Answer]) -> Quiz where Delegate.Question == Question, Delegate.Answer == Answer {
+        let flow = Flow(questions: questions, delegate: delegate, scoring:  { scoring($0, correctAnswers: correctAnswers) } )
+        flow.start()
+        return Quiz(flow: flow)
+    }
+}
