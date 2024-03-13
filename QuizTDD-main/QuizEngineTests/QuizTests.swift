66996a4ddad19ccfc31d8b792d3a8240a84bb7af
Vosik <vosikvos@gmail.com>
2024-03-14 01:43:00 +0200

    R - Updated new test to use new API

--- QuizTDD-main/QuizEngineTests/QuizTests.swift
+++ QuizTDD-main/QuizEngineTests/QuizTests.swift
@@ -10,53 +10,40 @@ import QuizEngine
 
 final class QuizTests: XCTestCase {
    
-    private var quiz: Quiz!
-    private let delegate = DelegateSpy()
-    
-    override func setUp() {
-        super.setUp()
-        quiz = Quiz.start(questions: ["Q1", "Q2"], delegate: delegate, correctAnswers: ["Q1": "A1", "Q2": "A2"])
-    }
-    
-    override func tearDown() {
-        quiz = nil
-        super.tearDown()
-    }
+    private var quiz: Quiz?
     
     func test_startQuiz_answerZeroOutOfTwoCorrectly_scoresZero() {
-        delegate.answerCompletion("wrong")
-        delegate.answerCompletion("wrong")
-        
-        XCTAssertEqual(delegate.handledResult!.score, 0)
-    }
-    
-    
-    func test_startQuiz_answerOneOutOfTwoCorrectly_scoresOne() {
-        delegate.answerCompletion("A1")
-        delegate.answerCompletion("wrong")
-        
-        XCTAssertEqual(delegate.handledResult!.score, 1)
-    }
-    
-    
-    func test_startQuiz_answerTwoOutOfTwoCorrectly_scoresTwo() {
+        let delegate = DelegateSpy()
+        quiz = Quiz.start(questions: ["Q1", "Q2"], delegate: delegate, correctAnswers: ["Q1": "A1", "Q2": "A2"])
         delegate.answerCompletion("A1")
         delegate.answerCompletion("A2")
         
-        XCTAssertEqual(delegate.handledResult!.score, 2)
+        XCTAssertEqual(delegate.completedQuizzes.count, 1)
+        assertEqual(delegate.completedQuizzes[0], [("Q1", "A1"), ("Q2", "A2")])
     }
     
     private class DelegateSpy: QuizDelegate {
         
         var answerCompletion: (String) -> Void = { _ in }
-        var handledResult: Result<String, String>? = nil
+        var completedQuizzes: [[(String, String)]] = []
         
-        func answer(for: String, completion: @escaping (String) -> Void) {
+        func answer(for question: String, completion: @escaping (String) -> Void) {
             self.answerCompletion = completion
         }
         
-        func handle(result: Result<String, String>) {
-            handledResult = result
+        func didCompleteQuiz(withAnswers answers: [(question: String, answer: String)]) {
+            completedQuizzes.append(answers)
         }
+        
+        func handle(result: QuizEngine.Result<String, String>) {}
+    }
+    
+    private func assertEqual(
+        _ a1: [(String, String)],
+        _ a2: [(String, String)],
+        file: StaticString = #filePath,
+        line: UInt = #line)
+    {
+        XCTAssertTrue(a1.elementsEqual(a2, by: ==), "\(a1) is not equal to \(a2)", file: file, line: line)
     }
 }
