883c49f6c15645410f6f929829b560272723b565
Vosik <vosikvos@gmail.com>
2024-03-18 00:48:23 +0200

    R - migrated the order test to new API

--- QuizAppTests/Presentation/Result/ResultsPresenterTest.swift
+++ QuizAppTests/Presentation/Result/ResultsPresenterTest.swift
@@ -91,14 +91,10 @@ final class ResultsPresenterTest: XCTestCase {
     
     func test_presentableAnswers_withTwoQuestions_mapsOrderedAnswer() {
         
-        let answers = [multipleAnswerQuestion: ["A2"], singleAnswerQuestion: ["A1", "A4"]]
-        let correctAnswers = [multipleAnswerQuestion: ["A2"], singleAnswerQuestion: ["A1", "A4"]]
+        let userAnswers = [(singleAnswerQuestion, ["A1", "A4"]), (multipleAnswerQuestion, ["A2"])]
+        let correctAnswers = [(singleAnswerQuestion, ["A1", "A4"]), (multipleAnswerQuestion, ["A2"])]
         
-        let orderedQuestions = [singleAnswerQuestion, multipleAnswerQuestion]
-        
-        let result = Result.make(answers: answers)
-        
-        let sut = ResultsPresenter(result: result, questions: orderedQuestions, correctAnswers: correctAnswers)
+        let sut = makeSUT(userAnswers: userAnswers, correctAnswers: correctAnswers)
         
         XCTAssertEqual(sut.presentableAnswers.count, 2)
         XCTAssertEqual(sut.presentableAnswers.first!.question, "Q1")
