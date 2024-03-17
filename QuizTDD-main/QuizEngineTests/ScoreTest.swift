494931f1761ab0109b4419a2ca76516e891e301a
Vosik <vosikvos@gmail.com>
2024-03-17 02:09:52 +0200

    R - improved names to clarify test intent

--- QuizTDD-main/QuizEngineTests/ScoreTest.swift
+++ QuizTDD-main/QuizEngineTests/ScoreTest.swift
@@ -14,28 +14,28 @@ final class ScoreTest: XCTestCase {
     }
 
     func test_oneWrongAnswer_scoresZero() {
-        XCTAssertEqual(BasicScore.score(for: ["wrong"], comparingTo: ["correct"]), 0)
+        XCTAssertEqual(BasicScore.score(for: ["not a match"], comparingTo: ["an answer"]), 0)
     }
     
-    func test_oneCorrectAnswer_scoresOne() {
-        XCTAssertEqual(BasicScore.score(for: ["correct"], comparingTo: ["correct"]), 1)
+    func test_oneMatchingAnswer_scoresOne() {
+        XCTAssertEqual(BasicScore.score(for: ["an answer"], comparingTo: ["an answer"]), 1)
     }
     
-    func test_oneCorrectAnswerOneWrong_scoresOne() {
+    func test_oneMatchingAnswerOneWrong_scoresOne() {
         
-        let score = BasicScore.score(for: ["correct 1", "wrong"], comparingTo: ["correct 1", "correct 2"])
+        let score = BasicScore.score(for: ["an answer", "not a match"], comparingTo: ["an answer", "another answer"])
         XCTAssertEqual(score, 1)
     }
     
-    func test_twoCorrectAnswers_scoresTwo() {
+    func test_twoMatchingAnswers_scoresTwo() {
         
-        let score = BasicScore.score(for: ["correct 1", "correct 2"], comparingTo: ["correct 1", "correct 2"])
+        let score = BasicScore.score(for: ["an answer", "another answer"], comparingTo: ["an answer", "another answer"])
         XCTAssertEqual(score, 2)
     }
     
-    func test_withUnequalSizeData_twoCorrectAnswers_scoresTwo() {
+    func test_withTooManyAnswers_twoMatchingAnswers_scoresTwo() {
         
-        let score = BasicScore.score(for: ["correct 1", "correct 2", "an extra answer"], comparingTo: ["correct 1", "correct 2"])
+        let score = BasicScore.score(for: ["an answer", "another answer", "an extra answer"], comparingTo: ["an answer", "another answer"])
         XCTAssertEqual(score, 2)
     }
     
