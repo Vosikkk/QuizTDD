e6f6a723613b0cdb8ac1522cd1bd6b3001398dab
Vosik <vosikvos@gmail.com>
2024-03-19 15:42:06 +0200

    F - introduced the new facotry method to create result view controllers

--- QuizApp/Routing/ViewControllerFactory.swift
+++ QuizApp/Routing/ViewControllerFactory.swift
@@ -9,7 +9,18 @@ import QuizEngine
 import UIKit
 
 protocol ViewControllerFactory {
+    
+    typealias Answers = [(question: Question<String>, answers: [String])]
+    
     func questionViewController(for question: Question<String>, answerCallback: @escaping ([String]) -> Void) -> UIViewController
     
+    func resultsViewController(for userAnswers: Answers) -> UIViewController
+    
     func resultViewController(for result: Result<Question<String>, [String]>) -> UIViewController
+}
+
+extension ViewControllerFactory {
+    func resultsViewController(for userAnswers: Answers) -> UIViewController {
+        UIViewController()
+    }
 }
