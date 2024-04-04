ad620fe917b8d0788c075b20819d9d08606e53a9
Vosik <vosikvos@gmail.com>
2024-04-05 01:18:04 +0300

    e - move QuizData to its own file

---
+++ QuizApp/Main/DemoQuizData.swift
@@ -0,0 +1,26 @@
+//
+//  DemoQuizData.swift
+//  QuizApp
+//
+//  Created by Саша Восколович on 05.04.2024.
+//
+
+import Foundation
+import QuizEngine
+
+let question1 = Question.singleAnswer("What's Mike's nationality?")
+let question2 = Question.multipleAnswer("What are Sasha's nationalities?")
+let questions = [question1, question2]
+let option1 = "Canadian"
+let option2 = "American"
+let option3 = "Ukrainian"
+let options1 = [option1, option2, option3]
+
+
+let option4 = "Canadian"
+let option5 = "American"
+let option6 = "Ukrainian"
+let options2 = [option4, option5, option6]
+let options = [question1: options1, question2: options2]
+
+let correctAnswers = [(question1, [option3]), (question2, [option5, option6])]
--- QuizApp/Main/SceneDelegate.swift
+++ QuizApp/Main/SceneDelegate.swift
@@ -60,20 +60,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 }
-
-let question1 = Question.singleAnswer("What's Mike's nationality?")
-let question2 = Question.multipleAnswer("What are Sasha's nationalities?")
-let questions = [question1, question2]
-let option1 = "Canadian"
-let option2 = "American"
-let option3 = "Ukrainian"
-let options1 = [option1, option2, option3]
-
-
-let option4 = "Canadian"
-let option5 = "American"
-let option6 = "Ukrainian"
-let options2 = [option4, option5, option6]
-let options = [question1: options1, question2: options2]
-
-let correctAnswers = [(question1, [option3]), (question2, [option5, option6])]
